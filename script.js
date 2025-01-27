3382efa68fad9812f841f849722b89ebed8f9b52
Ruhlan Rzayev <contactruhlanrzayev@gmail.com>
2025-01-27 22:58:45 +0400

    version2

--- script.js
+++ script.js
@@ -9,27 +9,6 @@ const loginPassword =  document.getElementById('loginPassword');
 const registerEmail = document.getElementById('registerEmail');
 const registerPassword = document.getElementById('registerPassword');
 const registerName = document.getElementById('registerName');   
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-apiKey: "AIzaSyDUGTCgGS-bUlo0-q-4yGls5vgsuCn9Keo",
-authDomain: "internintelligenceloginpage.firebaseapp.com",
-projectId: "internintelligenceloginpage",
-storageBucket: "internintelligenceloginpage.firebasestorage.app",
-messagingSenderId: "649603952250",
-appId: "1:649603952250:web:868cbdca5e51bc257255c3",
-measurementId: "G-XVMZ4KJ08S"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 function barsMenu() {
     navMenu.classList.toggle('active');
